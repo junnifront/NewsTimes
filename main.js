e61b43f3b3f59a70406b8ba3d9f149e922cffb5d
junnifront <jiyoonlee07@naver.com>
2024-07-09 21:31:33 +0900

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY=`ea9e3222412340b794c8f0e70e1677e0`;
 let news = [];
 const getLatestNews = async ()=> {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://newstimes-jiyoon.netlify.app/top-headlines?country=kr`
     );
     const response = await fetch(url);
     const data = await response.json();
